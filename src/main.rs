a1fb3e36da8327b1753a9fabba5029698b7f7d86
resqiar <resqiageng@gmail.com>
2023-10-25 21:35:33 +0700

    receive an args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let target_file = &args[2];
+
+    println!("{query}");
+    println!("{target_file}");
 }
